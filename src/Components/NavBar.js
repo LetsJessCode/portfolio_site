4240e9167ec45a52986aa810ebee0715311bc9a6
LetsJessCode <letsjesscode@gmail.com>
2020-11-03 12:27:02 -0500

    added route.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,13 @@ import {NavLink} from 'react-router-dom'
 const NavBar = () => {
     return (
         <nav>
-            
+            <ul>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/Bio">Bio Page</NavLink>
+                <NavLink to="/Projects"> Project Portfolio</NavLink>
+                <NavLink to="/Blogs">Blogs</NavLink>
+                <NavLink to="/Contact">Contact Info</NavLink>
+            </ul>
         </nav>
     )
 }
---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavBar } from './Components/NavBar'
+import { BrowswerRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+const Routes = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Routes
