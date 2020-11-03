80922c1c54d03e34b9fde9706e4fe38a6a2bd784
LetsJessCode <letsjesscode@gmail.com>
2020-11-03 12:58:21 -0500

    entered outline for landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,16 @@ const Home = () => {
     return (
         <div>
             <h3>Home Page</h3>
+            <ul>
+                <li> contact information  
+                    <ol>
+                        <li>email</li>
+                        <li>social media (twitter and linkedin)</li>
+                    </ol>
+                </li>      
+                <li> images of self  </li>        
+                <li> maybe a song to play </li>
+            </ul>
         </div>
     )
 }
