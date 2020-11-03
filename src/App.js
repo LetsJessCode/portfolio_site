fc053027a7df7bcc9e629058e6a183bf90f28fe9
LetsJessCode <letsjesscode@gmail.com>
2020-11-03 13:02:56 -0500

    setup routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import  NavBar  from './Components/NavBar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// ------------------------------
 import Home from './Components/Home'
+import Bio from './Components/Bio'
+import Projects from './Components/Projects'
+import Blogs from './Components/Blogs'
 // ------------------------------
 
 export class App extends Component {
@@ -12,7 +16,9 @@ export class App extends Component {
           <NavBar />
             <Switch>
                 <Route exact path="/" component={Home} />
-
+                <Route exact path="/bio" component={Bio} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/blogs" component={Blogs} />
             </Switch> 
         </Router>
       </div>
---
+++ src/Components/Bio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bio = () => {
+    return (
+        <div>
+            <h3>Bio Page</h3>
+        </div>
+    )
+}
+
+export default Bio
