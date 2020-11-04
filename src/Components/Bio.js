f5a18974ce12b2d69a82c69fbbe1402735efc869
LetsJessCode <letsjesscode@gmail.com>
2020-11-04 16:36:27 -0500

    started blog info

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -6,7 +6,12 @@ const Bio = () => {
             <h3>Bio Page</h3>
             <ul>
                 <li>Professional Image of self</li>
-                <li>Bio Paragraph</li>
+                <div className="bio"> 
+                <h4>Results-driven software engineer with a passion for helping people.</h4>
+                    <p>
+                        My natural skills to catch on quickly, along with my caring personality will allow me to always bring change to my work environment. My approach to changing the world is largely inspired by creating a better life for the next generation, which you’ll find reflected in my interactions with other and fight for social injustices. I believe that changing the world starts with changing the lives of those directly impacted by myself, and I hope that my positive influence will spread to those that I come in contact with.    
+                    </p> 
+                </div>
             </ul>
         </div>
     )
