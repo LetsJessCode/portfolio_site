4eac0e209b9e53794f0772418cbdc38422d68fef
LetsJessCode <letsjesscode@gmail.com>
2020-11-05 09:38:02 -0500

    added linkedin

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdEmail } from 'react-icons/md'
 import { CgTwitter } from 'react-icons/cg'
+import { GrLinkedin } from 'react-icons/gr'
 // ------------------------------
 
 const Home = () => {
@@ -11,7 +12,8 @@ const Home = () => {
                 <li> contact information  
                     <ol>
                     <a href="mailto:letsjesscode@gmail.com"><MdEmail /></a> <br />
-                     <a href="https://twitter.com/LetsJessCode"><CgTwitter /></a>
+                     <a href="https://twitter.com/LetsJessCode"><CgTwitter /></a> <br />
+                     <a href="https://www.linkedin.com/in/jessica-shearwood/"><GrLinkedin /></a>
                         <li>social media (twitter and linkedin)</li>
                     </ol>
                 </li>      
