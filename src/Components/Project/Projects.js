647827825646dda0fbd53c1a6195224f0cc41e16
LetsJessCode <letsjesscode@gmail.com>
2020-11-05 17:39:52 -0500

    added some explanation to textbook video

--- src/Components/Project/Projects.js
+++ src/Components/Project/Projects.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import { GrGithub } from 'react-icons/gr'
+import ReactPlayer from "react-player"
+
 import '../Project/Project.css'
 
 const Projects = () => {
     return (
         
         <div className="body">     
-            <h2 className="title">My Projects!</h2>
-                <ul>
-                    <li>Video WalkThroughs of Each Project</li>
+            <h2 className="title">My Projects! </h2>
+            
+            <div>
+                 <h3 className="textbook"> "TextBook Application" 
+                    <a href="https://github.com/LetsJessCode/textbook"><GrGithub /> </a>
+                </h3>
+                     <ReactPlayer className="video"
+                    url="https://youtu.be/UfLDn2Cxg4I" />
+                        <p> This Rails application allows you to create a user account, monitor what students have homework, and which subjects the assignments are in. As a student completes an assignment, it is removed from their homework list.</p>
+            </div>                
+            <div>
+                <h3> </h3>
+                <ReactPlayer className="video"
+                url="https://youtu.be/S5_O_5V4NAc" /> 
+            </div>
+            <div>
+            <h3></h3>
+                <ReactPlayer className="video"
+                url="https://youtu.be/CWqkws14XoA" />
+                
+            </div>
                     <li>Brief Summary of each project</li>
                     <li>Link to GitHub Repository</li>
-                </ul>
         </div>
     )
 }
